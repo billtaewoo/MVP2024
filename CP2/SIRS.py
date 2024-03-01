0d045ffcfa1fa4ff3066873f659b16f36986db64
TAEWOO LEE <billtaewoo@gmail.com>
2024-03-01 19:26:23 +0000

    2024-03-01 19:26:14

--- CP2/SIRS.py
+++ CP2/SIRS.py
@@ -91,8 +91,8 @@ def main():
 '''
 
 def main():
-    for prob1 in tqdm(np.arange(0, 1, 0.05)):  # increase prob1 by increment of 0.05.
-        for prob3 in tqdm(np.arange(0, 1, 0.05)):  # increase prob3 by increment of 0.05.
+    for prob1 in tqdm(np.arange(0, 1, 0.01)):  # increase prob1 by increment of 0.05.
+        for prob3 in tqdm(np.arange(0, 1, 0.01)):  # increase prob3 by increment of 0.05.
             size = 50  # size fixed as 50.
             prob2 = 0.5  # prob2 fixed as 0.5.
             lattice = initialize(size)  # initialized the lattice.
@@ -107,39 +107,10 @@ def main():
                 if n >= 100:
                     IperSwp = int(np.argwhere(lattice==1).shape[0]) # number of I on lattice per sweep.
                     TotalI += IperSwp  # add the IperSwp to Var. TotalI outside of loop.
-                #plt.cla()
-                #im = plt.imshow(lattice, animated=True)
-                #plt.draw()
-                #plt.pause(0.0001)
             averageI = TotalI / (nsteps-100)  # average I is totalI divided by number of sweep (1000 sweeps)
             print(averageI)
 
-            csv_wrtiter(prob1, prob3, averageI)    
-def main3():
-    prob1 = 0.4
-    prob3 = 0.3
-    size = 50  # size fixed as 50.
-    prob2 = 0.5  # prob2 fixed as 0.5.
-    lattice = initialize(size)  # initialized the lattice.
-    nsteps = 1100  # number of steps given.
-    TotalI = 0  # variable gets total number of I from IperSwp each sweep.
-
-    for n in tqdm(range(nsteps)):
-        # calling one sweep
-        for i in range(size * size):
-            lattice = SIR(size, lattice, prob1, prob2, prob3)  # p1 infection/ p2 recovery/ p3 susceptible again
-        # wait for equilibration
-        if n >= 100:
-            IperSwp = int(np.argwhere(lattice==1).shape[0]) # number of I on lattice per sweep.
-            TotalI += IperSwp  # add the IperSwp to Var. TotalI outside of loop.
-        #plt.cla()
-        #im = plt.imshow(lattice, animated=True)
-        #plt.draw()
-        #plt.pause(0.0001)
-    averageI = TotalI / (nsteps-100)  # average I is totalI divided by number of sweep (1000 sweeps)
-    print(averageI)
-
-    csv_wrtiter(prob1, prob3, averageI)  
+            csv_wrtiter(prob1, prob3, averageI)  # write out the plotting values to p1p3plot.csv
 
 if __name__ == "__main__":
-    main3()
+    main()
