2f0d049c8b4c2ca806c88a5e80da855bab828cca
TAEWOO LEE <billtaewoo@gmail.com>
2024-04-25 15:02:41 +0100

    1714053749

--- CP2/SIRS_new.py
+++ CP2/SIRS_new.py
@@ -12,17 +12,19 @@ class SIRS:
         self.p1 = prob1
         self.p2 = prob2
         self.p3 = prob3
+        # self.p_Im = prob_Im
         self.case = case
         self.measurements = int(measurements)
         self.nsteps = self.measurements * 10 + 100 # measurement * decorrelation time * equilibration time
-        if case == "SIRS":
-            self.lattice = np.random.choice([0,1,2], size=(self.size,self.size))
-        elif case == "Immune":
-            self.lattice = np.random.choice([0,1,2], size=(self.size,self.size))
-            pop = int((self.size **2) * prob_Im)
-            x = np.random.randint(0, self.size, size = pop)
-            y = np.random.randint(0, self.size, size = pop)
-            self.lattice[y,x] = 3 #make it immune
+        self.lattice = np.random.choice([0,1,2], size=(self.size,self.size))
+        # if case == "SIRS":
+        #     self.lattice = np.random.choice([0,1,2], size=(self.size,self.size))
+        # elif case == "Immune":
+        #     self.lattice = np.random.choice([0,1,2], size=(self.size,self.size))
+        #     pop = int((self.size **2) * self.p_Im)
+        #     x = np.random.randint(0, self.size, size = pop)
+        #     y = np.random.randint(0, self.size, size = pop)
+        #     self.lattice[y,x] = 3 #make it immune
 
     def updates(self):
         iteration = int(self.size ** 2)
@@ -32,17 +34,30 @@ class SIRS:
         for i in range(iteration):
             X = x[i]
             Y = y[i]
-            neighbour = (self.lattice[(Y+1)%self.size,X]+
-                        self.lattice[(Y-1)%self.size,X]+
-                        self.lattice[Y,(X+1)%self.size]+
-                        self.lattice[Y,(X-1)%self.size])
+            up = self.lattice[(Y+1)%self.size,X]
+            down = self.lattice[(Y-1)%self.size,X]
+            right = self.lattice[Y,(X+1)%self.size]
+            left = self.lattice[Y,(X-1)%self.size]
             if self.lattice[Y,X] == 0 :
-                if neighbour >= 1:
+                if up == 1:
+                    self.lattice[Y,X] = random.choices([1,0], weights=[self.p1,1-self.p1], k=1)[0]
+                elif down == 1:
+                    self.lattice[Y,X] = random.choices([1,0], weights=[self.p1,1-self.p1], k=1)[0]
+                elif right == 1:
+                    self.lattice[Y,X] = random.choices([1,0], weights=[self.p1,1-self.p1], k=1)[0]
+                elif left == 1:
                     self.lattice[Y,X] = random.choices([1,0], weights=[self.p1,1-self.p1], k=1)[0]
             elif self.lattice[Y,X] == 1:
                 self.lattice[Y,X] = random.choices([2,1], weights=[self.p2,1-self.p2], k=1)[0]
             elif self.lattice[Y,X] == 2:
                 self.lattice[Y,X] = random.choices([0,2], weights=[self.p3,1-self.p3], k=1)[0]
+    
+    def immune(self):
+        pop = int((self.size **2) * self.p_Im)
+        x = np.random.randint(0, self.size, size = pop)
+        y = np.random.randint(0, self.size, size = pop)
+        self.lattice[y,x] = 3 #make it immune
+
     
     def animate(self):
         fig, ax = plt.subplots()
@@ -61,3 +76,10 @@ class SIRS:
         vars = np.var(sets, axis = 0)
         error = np.std(vars, axis= 0)
         return error
+
+def main():
+    x = SIRS(50,100,0.6,0.5,0.1)
+
+    x.animate()
+
+main()
