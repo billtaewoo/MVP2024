044c84d3c026a7eb43941bb92dbdadbf9d77ffda
TAEWOO LEE <billtaewoo@gmail.com>
2024-05-01 15:18:24 +0100

    1714573089

--- CP2/COM.plt
+++
@@ -1,4 +0,0 @@
-set title "Center of Mass versus time for glider"
-set xlabel "time"
-set ylabel "center of mass"
-plot "gliderCOM.dat"
--- CP2/GameOfLife.py
+++ CP2/GameOfLife.py
@@ -10,7 +10,6 @@ class GameofLife:
     def __init__(self, size, type, measurement):
         self.size = int(size)
         self.measurement = int(measurement)
-        self.nsteps = measurement * 100
         if type == "random":
             self.lattice = np.random.choice([0,1],size=(self.size, self.size))
         elif type == "oscillators":
@@ -23,8 +22,10 @@ class GameofLife:
                 self.lattice[y, (x+1)%self.size] = 1
         elif type == "gliders":
             self.lattice = np.zeros((size,size))
-            x = np.random.randint(0, self.size)
-            y = np.random.randint(0, self.size)
+            #x = np.random.randint(0, self.size)
+            #y = np.random.randint(0, self.size)
+            x = 0
+            y = 0
             if self.lattice[y,x] == 0:
                 self.lattice[(y-1)%self.size,x] = 1
                 self.lattice[y,(x+1)%self.size] = 1
@@ -45,34 +46,36 @@ class GameofLife:
     def animate(self):
         fig, ax = plt.subplots()
         im = ax.imshow(self.lattice)
-        for i in range(self.nsteps):
+        for i in range(self.measurement):
             self.rule()
             im.set_array(self.lattice)
             plt.pause(0.0001)
         plt.show()
 
         
-    def measure(self):
-        time = 0
+    def measure(self, maxnruns = 2000):
         previous = np.sum(self.lattice)
         counter = 0
-        for i in range(self.nsteps):
+        for i in range(maxnruns):
             self.rule()
             current = np.sum(self.lattice)
             if current == previous:
                 counter += 1
             if counter > 50:
-                time = i - 50
+                
                 # reset lattice before next measure
                 self.lattice = np.random.choice([0,1],size=(self.size, self.size))
-                break
+                return  i - 50
+                
             previous = current
-        return time
+        # reset lattice before next measure
+        self.lattice = np.random.choice([0,1],size=(self.size, self.size))
+        return i
     
-    def histogram(self):
+    def histogram(self, maxnruns = 2000):
         times = np.zeros(self.measurement)
         for i in tqdm(range(self.measurement)):
-            time = self.measure()
+            time = self.measure(maxnruns=maxnruns)
             times[i] = time
         data = {"time":[]}
         for j in range(self.measurement):
@@ -81,9 +84,38 @@ class GameofLife:
         df = pd.DataFrame(data)
         df.to_csv("histogram.csv",index=True)
         print(f"Data saved successfully!")
+        plt.hist(times, bins = 50)
+        plt.show()
+    
+    def Com(self):
+        m_sums = np.sum(np.argwhere(self.lattice == 1), axis = 0)
+        com = m_sums/np.sum(self.lattice)
+        return com
+        
+    def Com_Calc(self, limit=51):
+   
+        data = {"time":[],"com_x":[],"com_y":[], "velocity":[]}
+        for i in range(limit):
+            self.rule()
+            com = self.Com()
+            if i > 1:
+                data["com_x"].append(com[0])
+                data["com_y"].append(com[1])
+                data["time"].append(i)
+                data["velocity"].append(np.sqrt(com[0]**2+com[1]**2)/i)
+
+        df = pd.DataFrame(data)
+        df.to_csv("Center_Mass.csv",index=True)
+        print(f"Data saved successfully!")
+        plt.scatter(data["com_x"], data["com_y"])
+        plt.show()
+        vel = np.mean(data["velocity"])
+        print("velocity of the glider is " + str(vel))
+
 
 def main():
-    x= GameofLife(50,"random",1000)
+    x= GameofLife(50,"gliders", 1000)
     # x.animate()
-    x.histogram()
+    # x.histogram()
+    x.Com_Calc()
 main()
